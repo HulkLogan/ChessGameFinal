a6d85a8ebb09923f98c32c4ddb5ead724a76a976
mechanicalmaniac <deusexmadmachina@gmail.com>
2016-03-21 18:50:31 -0400

    Update ChessModel.java

--- ChessGameWorking/src/chess/ChessModel.java
+++ ChessGameWorking/src/chess/ChessModel.java
@@ -106,6 +106,7 @@ public class ChessModel implements IChessModel {
 			}
 			board[move.toRow][move.toColumn] = board[move.fromRow][move.fromColumn];
 			board[move.fromRow][move.fromColumn] = null;
+			board[move.toRow][toColumn].isNowMoved(); 
 			setNextPlayer();
 		}
 		else {
