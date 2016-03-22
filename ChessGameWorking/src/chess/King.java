e9048d9958dba4e2ed97e9b78d5ff7747cd86775
mechanicalmaniac <deusexmadmachina@gmail.com>
2016-03-21 23:46:06 -0400

    Update King.java

--- ChessGameWorking/src/chess/King.java
+++ ChessGameWorking/src/chess/King.java
@@ -18,7 +18,20 @@ private Player owner;
 	}
 	
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
+		int firstRank = (this.owner == Player.WHITE) ? 0 : 7;
 		if(super.isValidMove(move, board)) {
+			if (!this.hasMoved && Math.abs(move.toColumn - move.fromColumn) == 2 &&
+				move.toRow == firstRank)
+				boolean kingside == move.toColumn > 4;
+				if (!(ChessPiece) board[firstRank][0].hasMoved()) {
+					for (int n = 4; n != kingside ? 8 : -1; n += kingside ? 1 : -1)
+					{
+						if (board[firstRank][n] != null || (ChessPiece)
+							|| (board[firstRank][n].isThreatened() &&
+							Math.abs(n-4) <= 2))
+							return false;
+					}
+				}
 			if (Math.abs(move.toRow - move.fromRow) > 1 || 
 					Math.abs(move.toColumn - move.fromColumn) > 1) {
 				return false;
